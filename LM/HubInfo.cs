768f32ceebfbe314cbf954029aa84335f2bd4292
Tsukiyosi <tsukiyosiae@gmail.com>
2023-12-19 18:30:35 +0700

    fixed connection problems, added hub colliction, fixed problem with ading messages to listbox

---
+++ LM/HubInfo.cs
@@ -0,0 +1,28 @@
+﻿using LM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LM
+{
+    class HubInfo
+    {
+        private ObservableCollection<MessageInfo> earlyMessages;
+        private string hubLink;
+
+        public ObservableCollection<MessageInfo> EarlyMessages
+        {
+            get { return earlyMessages; }
+            set { earlyMessages = value; }
+        }
+        public string HubLink
+        {
+            get { return hubLink; }
+            set { hubLink = value; }
+        }
+
+    }
+}
